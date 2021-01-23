4a2caaa50ebdc8b5f22a3c5599123e242455cb7b
ahmet92akinsel <ahmet.akinsel@gmail.com>
2021-01-23 16:40:42 +0200

    name,age property and growUp()

--- src/pet.js
+++ src/pet.js
@@ -1,5 +1,13 @@
-function Pet() {
-    return Pet.name;
-};
+function Pet(name) {
+    this.name = name;
+    this.age = 0;
+    this.growUp = function() {
+        this.age += 1;
+      };
+    };
 
-module.exports = Pet;
+
+
+
+module.exports =
+Pet;
