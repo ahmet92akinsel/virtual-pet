a5254bfbb9cbb5bf145a07a406d0e7df2a347745
ahmet92akinsel <ahmet.akinsel@gmail.com>
2021-01-23 17:25:13 +0200

    virtual-pets hunger and fitness property

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -9,14 +9,14 @@ describe('constructor', () => {
         const pet = new Pet('Fido');
     
         expect(pet.name).toEqual('Fido');
-      });
+    });
       it('has a initial age of 0', () => {
         const pet = new Pet('Fido');
     
         expect(pet.age).toEqual(0);
       });
     });
-    describe('growUp', () => {
+describe('growUp', () => {
       it('increments the age by 1', () => {
         const pet = new Pet('Fido');
     
@@ -25,4 +25,16 @@ describe('constructor', () => {
         expect(pet.age).toEqual(1);
       });
     });
+      it('increase the hunger', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.hunger).toBe(5);
+      });
+
+      it('decrease the fitness', () =>{
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.fitness).toBe(7)
+      });
+    
      
